7f87f07d854b88fc488aa543176ff061ea43c946
PalstarKe <maishwriter2@gmail.com>
2023-12-08 17:06:58 +0300

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metedata = {
+  title: "Promtify",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <Nav />
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
